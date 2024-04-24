be1f6be2a10ceb2c1a52c2da8ee5a0d1add0d6d4
Kim MinChan <163220362+Lakimas@users.noreply.github.com>
2024-04-24 01:54:46 +0000

    중간고사 끝!

---
+++ controllers/homeController.js
@@ -0,0 +1,24 @@
+// homeController.js
+"use strict";
+
+/**
+ * Listing 12.5 (p. 178)
+ * 홈 컨트롤러로의 라우팅
+ */
+exports.showHome = (req, res) => {
+    res.render("index");
+}
+exports.showtrans = (req, res) => { // 특정 라우트를 위한 콜백 함수 추가
+    res.render("transportation");
+};
+exports.showSignUp = (req, res) => {
+    res.render("contact");
+};
+exports.postedSignUp = (req, res) => {
+    res.render("thanks");
+};
+
+/**
+ * Listing 12.9 (p. 182)
+ * 콘텐츠의 설정과 렌더링된 뷰에 전달
+*/
